84622b2e8621f1b7623623094c7fd3b8a03b60a2
Leonardo Rufino <119766870+leorufinx@users.noreply.github.com>
2024-04-16 17:36:41 -0300

    commit

--- exercícios_seção05/ex7.py
+++ exercícios_seção05/ex7.py
@@ -5,7 +5,9 @@ numero2 = int(input('digite outro numero: '))
 
 if numero1 > numero2:
     print('numero maior: ', numero1)
+
 elif numero2 > numero1:
     print('numero maior:', numero2)
+    
 elif numero1 == numero2:
     print('numeros iguais')
